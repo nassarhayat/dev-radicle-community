915eb446d96dd1f6ea0f738aa719ae0a89031b41
Nassar Hayat <nassar@makerlabs.co.uk>
2021-11-01 17:12:39 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<div class="seedling">🌱</div>
+	<h1>Create a DevDAO, join a DevDAO, start contributing to DAOs as a developer via <a href="https://twitter.com/radicle" target="_blank">@radicle</a></h1>
+	<form action="https://l57by8cmnip.typeform.com/to/zLsgh566" target="_blank">
+		<input class="button" type="submit" value="Create DevDAO" />
+	</form>
+</main>
+
+<style>
+	main {
+		padding: 4em;
+		background-color: black;
+		height: 85vh;
+	}
+
+	h1 {
+		color: white;
+		font-size: 4em;
+		font-weight: 500;
+	}
+
+	.seedling {
+		font-size: 4rem;
+	}
+
+	.button {
+		background-color: red;
+		color: white;
+		height: 60px;
+		width: 180px;
+		border-radius: 30px;
+		border: 0;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
