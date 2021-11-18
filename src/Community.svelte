401f1251212fc2cb1f1328e241cea2dce7f84b2f
Brandon Oxendine <brandon.oxendine@gmail.com>
2021-11-18 23:04:07 +0100

    add and style logos

--- src/Community.svelte
+++ src/Community.svelte
@@ -1,12 +1,18 @@
 <main>
   <h2>Fast growing community</h2>
   <h3>Join top DevDAOs.</h3>
-  <div class="cardrow">
-    <div class="card">
-      <h4 class="cardtitle">Ecosystems</h4>
-    </div>
-    <div class="card">
-      <h4 class="cardtitle">DevDAOs</h4>
-    </div>
+  <div id="logos">
+    <img src="/assets/images/akash.png" alt="akash logo" />
+    <img src="/assets/images/ceramic.png" alt="ceramic logo" />
+    <img src="/assets/images/dxdao.png" alt="dxdao logo" />
+    <img src="/assets/images/gnosis.png" alt="gnosis logo" />
+    <img src="/assets/images/makerdao.png" alt="makerdao logo" />
+    <img src="/assets/images/polygon.png" alt="polygon logo" />
+    <img src="/assets/images/snapshot.png" alt="snapshot logo" />
+    <img src="/assets/images/synthetix.png" alt="synthetix logo" />
+    <img src="/assets/images/textile.png" alt="textile logo" />
+    <img src="/assets/images/thegraph.png" alt="the graph logo" />
+    <img src="/assets/images/uma.png" alt="uma logo" />
+    <img src="/assets/images/walletconnect.png" alt="walletconnect logo" />
   </div>
 </main>
