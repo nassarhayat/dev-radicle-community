d6b4c16122e2bdd837a62899889c37aa53e5ebef
Brandon Oxendine <brandon.oxendine@gmail.com>
2021-11-22 14:35:26 -0500

    add profile, org, pullpush, githubgitlab images, add active state for button
    Signed-off-by: Brandon Oxendine <brandon.oxendine@gmail.com>

--- src/Distribute.svelte
+++ src/Distribute.svelte
@@ -5,11 +5,11 @@
     <div class="card org-screenshot">
       <h4 class="cardtitle">Org</h4>
     </div>
-    <div class="card">
-      <h4 class="cardtitle">Pull/push</h4>
+    <div class="card pullpush-screenshot">
+      <h4 class="cardtitle">Pull & push</h4>
     </div>
-    <div class="card">
-      <h4 class="cardtitle">github/gitlab mirroring</h4>
+    <div class="card githubgitlab-screenshot">
+      <h4 class="cardtitle">Github & Gitlab mirroring</h4>
     </div>
   </div>
 </main>
