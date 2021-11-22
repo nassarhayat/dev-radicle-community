726aae1ce08b17f68f11d6ffc5cc7b00e4fcbe57
Brandon Oxendine <brandon.oxendine@gmail.com>
2021-11-22 09:45:20 -0500

    style cards
    Signed-off-by: Brandon Oxendine <brandon.oxendine@gmail.com>

--- src/Distribute.svelte
+++ src/Distribute.svelte
@@ -2,7 +2,7 @@
   <h2>Code Distribution</h2>
   <h3>Decentralise your operational stack.</h3>
   <div class="cardrow">
-    <div class="card">
+    <div class="card org-screenshot">
       <h4 class="cardtitle">Org</h4>
     </div>
     <div class="card">
