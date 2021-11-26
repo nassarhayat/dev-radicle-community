9e4d00499a261d102ca74c82d6d99205def36b70
brandonhaslegs <brandon.oxendine@gmail.com>
2021-11-26 17:40:17 -0500

    add how it works
    Signed-off-by: brandonhaslegs <brandon.oxendine@gmail.com>

--- src/HIW.svelte
+++ src/HIW.svelte
@@ -1,10 +1,5 @@
 <main>
   <h2>How it works</h2>
   <h3>Peer to peer tech with an open protocol architecture.</h3>
-  <div class="card">
-    <div class="text">
-      <h4 class="cardtitle">How it works</h4>
-      <p>This is how Radicle works.</p>
-    </div>
-  </div>
+  <div class="card howitworks-screenshot" />
 </main>
